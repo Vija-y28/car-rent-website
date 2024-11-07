d51440ebb4209848cd439244d0eb4ddf795c0734
Challa Vijay <challavijay222@gmail.com>
2024-11-07 21:13:54 +0530

    Add files via upload

---
+++ Car Rent/car.js
@@ -0,0 +1,10 @@
+const sr=ScrollReveal({
+    distance:'80px',
+    duration:2500,
+    delay:400,
+    reset:true
+
+});
+
+sr.reveal('.home-bar',{delay:200, origin:'top'})
+sr.reveal('.work',{delay:200, origin:'top'})
